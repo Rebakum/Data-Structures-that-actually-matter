dca98b069d01181e80e8d7467967ee1ebc5ac815
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-11-05 08:07:33 +0600

    linkedliist prepend

---
+++ M03V08.js
@@ -0,0 +1,80 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    //if the linked list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //if the linked list is not empty
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  prepend() {
+    const newNode = new Node(value);
+    //if the linked list is empty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //if the linked list is not empty
+      newNode.next = this.head;
+      this.head = newNode();
+    }
+    this.length++;
+  }
+
+  insert() {}
+
+  remove() {}
+
+  // print() {
+  //   let currentNode = this.head;
+  //   while (currentNode !== null) {
+  //     console.log(currentNode.value);
+  //     currentNode = currentNode.next;
+  //   }
+  // }
+  //output
+  // 1
+  // 2
+  // 3
+  // 4
+  //or
+  print() {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(arr.join("->"), "-> null");
+    //output
+    // 1->2->3->4 -> null
+  }
+}
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+
+linkedList.prepend(10);
+linkedList.prepend(20);
+linkedList.prepend(30);
+linkedList.print();
