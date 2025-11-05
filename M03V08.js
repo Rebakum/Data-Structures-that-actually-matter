a23fb5bc2a05d4aebabd470387b7ce8370c51d36
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-11-05 11:43:48 +0600

    ligit add .

--- M03V08.js
+++ M03V08.js
@@ -25,7 +25,7 @@ class LinkedList {
     this.length++;
   }
 
-  prepend() {
+  prepend(value) {
     const newNode = new Node(value);
     //if the linked list is empty
     if (this.head === null) {
@@ -34,12 +34,39 @@ class LinkedList {
     } else {
       //if the linked list is not empty
       newNode.next = this.head;
-      this.head = newNode();
+      this.head = newNode;
     }
     this.length++;
   }
 
-  insert() {}
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      console.error("index out of bound");
+      return undefined;
+    }
+    // if the insert is in the start of the linked list
+    if (index === 0) {
+      return this.prepend(value);
+    }
+    //if the insert is in the end  of the list
+    if (index === this.length) {
+      return this.append(value);
+    }
+    //if the insert is in the middle
+    //find the leading node
+    let count = 0;
+    let leadingNode = this.head;
+
+    while (count !== index - 1) {
+      leadingNode = leadingNode.next;
+      count++;
+    }
+    console.log(leadingNode);
+    // Node {
+  // value: 20,
+  // next: Node { value: 10, next: Node { value: 1, next: [Node] } }
+}
+  }
 
   remove() {}
 
@@ -65,7 +92,8 @@ class LinkedList {
     }
     console.log(arr.join("->"), "-> null");
     //output
-    // 1->2->3->4 -> null
+    // 1. appened 1->2->3->4 -> null
+    // 2.prepend 30->20->10->1->2->3->4 -> null
   }
 }
 const linkedList = new LinkedList();
@@ -77,4 +105,6 @@ linkedList.append(4);
 linkedList.prepend(10);
 linkedList.prepend(20);
 linkedList.prepend(30);
+linkedList.insert(2, 100);
+
 linkedList.print();
